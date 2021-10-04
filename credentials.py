3aba62e2f531f900675b78f145773223c2dccf7e
theov10 <theo.vercruyssen@gmail.com>
2021-10-04 11:38:54 +0200

    update gitignore and cridentials to .env

---
+++ credentials.py
@@ -0,0 +1,10 @@
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+ALP_API_ID=os.getenv("ALP_API_ID")
+ALP_SECRET_KEY=os.getenv("ALP_SECRET_KEY")
+NEWS_API_KEY=os.getenv("NEWS_API_KEY")
